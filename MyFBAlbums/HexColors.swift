abec5b19da7eebeb24cde3a7ba7381b31f478d87
Home <Home@MacBook-Pro-de-Othmane.local>
2017-10-22 19:03:24 +0100

    first commit

---
+++ MyFBAlbums/HexColors.swift
@@ -0,0 +1,9 @@
+//
+//  HexColors.swift
+//  MyFBAlbums
+//
+//  Created by Home on 22/10/2017.
+//  Copyright © 2017 OthmaneOuenzar. All rights reserved.
+//
+
+import Foundation
---
+++ MyFBAlbums/PhotoTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoTableViewCell.swift
+//  MyFBAlbums
+//
+//  Created by Home on 22/10/2017.
+//  Copyright © 2017 OthmaneOuenzar. All rights reserved.
+//
+
+import Foundation
